628bcc48863a13f4391cbea20de1bdf0a5eda440
Michal Rosa <michal.rosa@kiwi.com>
2020-01-25 18:46:33 +0100

    checkout page, checkout item component and updated cart with selectors

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import React from 'react';
 import './App.css';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component.jsx';
 import Header from './components/header/header.component';
 import SignInSignUpPage from './pages/sign-in-sign-up/sign-in-sign-up.components';
+import CheckoutPage from './pages/checkout/checkout.component';
+
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 import { setCurrentUser } from './redux/user/user.actions';
+import { selectCurrentUser } from './redux/user/user.selector';
 
 class App extends React.Component {
   unsubscribeFromAuth = null;
@@ -47,6 +51,7 @@ class App extends React.Component {
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route path="/shop" component={ShopPage} />
+          <Route exact path="/checkout" component={CheckoutPage} />
           <Route
             exact
             path="/signin"
@@ -64,8 +69,8 @@ class App extends React.Component {
   }
 }
 
-const mapStateToProps = ({ user }) => ({
-  currentUser: user.currentUser
+const mapStateToProps = createStructuredSelector({
+  currentUser: selectCurrentUser
 });
 
 const mapDispatchToProps = dispatch => ({
--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -1,17 +1,34 @@
 import { createSelector } from 'reselect';
 
-
 const selectCart = state => state.cart;
 
+export const selectCartHidden = createSelector(
+  [selectCart],
+  cart => cart.hidden
+);
+
 export const selectCartItems = createSelector(
-    [selectCart],
-    cart => cart.cartItems
+  [selectCart],
+  cart => cart.cartItems
 );
 
 export const selectCartItemsCount = createSelector(
-    [selectCartItems],
-    cartItems => cartItems.reduce((accumalatedQuantity, cartItem) => accumalatedQuantity + cartItem.quantity, 0)
-    // reduce starts from 0 and then accultaing base on + operator every another quaintity in cart
-    // This is computing a new value based on state -> called a selector.. there is a issue with performace (if its placed in maptostate, its called everytime)
-    // -> thats why we use memoization (special form of caching, returning value stored in cache) - wuth library 'Reselect'..and thats why we created this file
+  [selectCartItems],
+  cartItems =>
+    cartItems.reduce(
+      (accumalatedQuantity, cartItem) =>
+        accumalatedQuantity + cartItem.quantity,
+      0
+    )
+  // reduce starts from 0 and then accultaing base on + operator every another quaintity in cart
+  // This is computing a new value based on state -> called a selector.. there is a issue with performace (if its placed in maptostate, its called everytime)
+  // -> thats why we use memoization (special form of caching, returning value stored in cache) - wuth library 'Reselect'..and thats why we created this file
+);
+
+export const selectCartTotal = createSelector([selectCartItems], cartItems =>
+  cartItems.reduce(
+    (accumalatedQuantity, cartItem) =>
+      accumalatedQuantity + cartItem.quantity * cartItem.price,
+    0
+  )
 );
