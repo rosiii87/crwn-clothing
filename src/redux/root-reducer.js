32c1faaf8954d82cb9ac5973b3528f2a0900d641
Michal Rosa <michal.rosa@kiwi.com>
2020-01-25 22:32:28 +0100

    refactoring shop and category to redux reduceres

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,9 +1,24 @@
 import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+// technicaly say, use LocalStorage
 
 import userReducer from './user/user.reducer';
 import cartReducer from './cart/cart.reducer';
+import directoryReducer from './directory/directory.reducer';
+import shopReducer from './shop/shop.reducer';
 
-export default combineReducers({
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['cart'] //because user is dealt by firebase
+};
+
+const rootReducer = combineReducers({
   user: userReducer,
-  cart: cartReducer
+  cart: cartReducer,
+  directory: directoryReducer,
+  shop: shopReducer
 });
+
+export default persistReducer(persistConfig, rootReducer);
